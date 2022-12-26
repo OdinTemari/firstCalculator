cdb853e079b428a9d7d20d68938e1e91e0a84cc4
Odin Temari <odintemari@gmail.com>
2022-12-26 18:55:57 +0300

    Operations working:

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function doMath(a, b, operand) {
+function doMath (arr) { //(a, b, operand) { //
     // function executes the math operation given input numbers and operand
-    if (operand.includes('+')) {
+    a= arr[0]; b=arr[1]; operand=arr[2];
+    if (!operand || operand == "") {
+        // only first number passed
+        return a;
+    } else if (operand.includes('+')) {
         return a+b;
     } else if (operand.includes('-')) {
         return a-b;
@@ -14,17 +18,46 @@ function doMath(a, b, operand) {
 }
 
 function getInputs(string) {
+    string = string.replace (' ', '');
+    let str= string.split(/(?=\-)|(?=\+)|(?=\*)|(?=\/)/g)
+    if (str.length<2) {
+        // Only one number found
+        return [Number(str[0]), [], ''];
+    }
+    ops= str[1].charAt(0);
+    str[1]= str[1].replace(str[1].charAt(0), '');
+    return [Number(str[0]), Number(str[1]), ops];
+}
+
+
+function getInputsOld(string) {
     // reads the input string and splits to get componenets
     string= string.replace(' ','');
-    let nums= string.split(/[+-\/*]/gm);
+    let nums= string.split(/[+-\/\*]/); //(/(?=[-+*\/])/); //string.split(/[+-\/*]/gm);
     let ops = string.split(/[\d]/);
-    if (ops.length<2) {
-        return "Operation Error";
-    } else if (ops.length<0) {return 'noops'};
+    
+    if (ops.includes('*')) {
+        ops='*'
+    } else if (ops.includes('/')) {
+        ops= '/';
+    } else if (ops.includes ('-')) {
+        ops= '-';
+    } else if (ops.includes ('+')) {
+        ops='+';
+    } else {
+        ops="";
+    }
+    // if (ops.length<2) {
+    //     return "Operation Error";
+    // } else if (ops.length<0) {return 'noops'};
     let returnarray = [Number(nums[0]), 
         Number(nums[1]),
-        ops[1]];
+        ops];
     return returnarray;
+}
+
+function appendBtnText() {
+    this
 }
 
 function calcualte(inputstr) {
@@ -32,11 +65,28 @@ function calcualte(inputstr) {
     if (typeof inputs[0]=='string') {
         console.log(inputs);
         return;
+    } else if (nums[1]=='NaN') {
+        //only first input detected (no operand likely)
+        return num[1];
     } else {
         let result= doMath(inputs[0], inputs[1], inputs[2]);
         console.log(result);
     }
 }
+
+///
+// window.addEventListener("keydown", function (e) {
+//     const idstr = ".keynum" + e.keyCode;
+//     let audio = document.querySelector(idstr);
+//     let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+//     if (!audio) return; // skip if no associated audio data
+//     audio.currentTime = 0;
+//     audio.play();
+
+
+///
+
+
 
 // create number pads on the interface
 const buttonpanel= document.getElementById('numpad-panel');
@@ -44,7 +94,7 @@ const text_table= [
     '7', '8', '9', '*',
     '4', '5', '6', '/',
     '1', '2', '3', '+',
-    '0', 'CLR', '', '-'];
+    '0', 'CLR', '=', '-'];
     
 for (let n=0; n<16; n++) {
     // create 16 buttons for nums and operators
@@ -52,17 +102,33 @@ for (let n=0; n<16; n++) {
     btn.classList.add('button');
     btn.textContent=text_table[n];
     if (Number(text_table[n])) {
-        btn.addEventListener('click', function() {
+        btn.addEventListener('click', function(e) {
             // append number to display box
+            let display= document.getElementById('display');
+            display.value= display.value+
+                this.textContent;
         })
     } else if (text_table[n]==('CLR')) {
         btn.addEventListener('click', function() {
             // clear display funciton
+            let display= document.getElementById('display');
+            display.value='';
         })
     } else {
         // operand buttons
         // evaluate current content in display and
         // append current operand
+        btn.addEventListener('click', function() {
+            let display = document.getElementById('display');
+            let input= getInputs(display.value);
+            let value= doMath(input);
+            if (value%1 !== 0) {
+                value= value.toFixed(3);
+            }
+            display.value= `${value}`+this.textContent;
+        }
+        );
+        
 
     }
     
